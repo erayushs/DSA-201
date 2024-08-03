00f8f06b0fdc8f9d3d9fcdd0f1f428a0b0184648
Ayush Singh <anmol8960@gmail.com>
2024-08-03 18:15:47 +0530

    optimized

--- practice.js
+++ practice.js
@@ -1,5 +1,6 @@
-//NEXT GREATER ELEMENT
+//NEXT GREATER ELEMENT Time Ccomplexity = O(n²);
 
+/*
 let arr = [1, 3, 2, 4]; // Ans = 3, 4, 4, -1
 
 let n = arr.length;
@@ -17,6 +18,27 @@ for (let i = 0; i < n; i++) {
 
   if (!found) {
     result.push(-1);
+  }
+}
+
+console.log(result);
+*/
+
+let arr = [1, 3, 2, 4];
+
+let n = arr.length;
+
+let result = new Array(n).fill(-1);
+let stack = [];
+
+stack.push(arr[0]);
+
+for (let i = 1; i < n; i++) {
+  if (stack[stack.length - 1] < arr[i]) {
+    result.shift();
+    result.push(arr[i]);
+    stack.pop();
+    stack.push(arr[i]);
   }
 }
 
--- run.js
+++ run.js
@@ -1,21 +1,21 @@
 // Next greater element
 let arr = [1, 3, 2, 4];
+let n = arr.length;
 
 let stack = [];
+let result = new Array(n).fill(-1);
+
 stack.push(0);
 
-let result = new Array(n);
-
-for (let i = 1; i < arr.length; i++) {
-  let x = arr[i];
+for (let i = 1; i < n; i++) {
   if (stack.length == 0) {
     stack.push(i);
     continue;
   }
   let topIdx = stack[stack.length - 1];
 
-  while (stack.length && x > arr[topIdx]) {
-    result[topIdx] = x;
+  while (stack.length && arr[i] > arr[topIdx]) {
+    result[topIdx] = arr[i];
     // x is NGE of top of stack
     stack.pop();
     topIdx = stack[stack.length - 1];
@@ -24,9 +24,9 @@ for (let i = 1; i < arr.length; i++) {
   stack.push(i);
 }
 
-while (stack.length > 0) {
-  let topIdx = stack.pop();
-  result[topIdx] = -1;
-}
+// while (stack.length > 0) {
+//   let topIdx = stack.pop();
+//   result[topIdx] = -1;
+// }
 
-// return result;
+console.log(result);
